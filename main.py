2b3c21b27403fe7ba560712b71e9322214a596ee
ewn179 <127596474+ewn179@users.noreply.github.com>
2023-05-28 18:25:25 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,63 +1,59 @@
-import os # модуль для управления системного администрирования
-import time # модуль, позволяющий устанавливать задержки в реальном времени (секундах)
-import random # модуль для случайного выбора
-import asyncio # модуль асинхронного программирования
-from telegram import Bot # модуль из скаченной библиотеки Telegram
+import os
+import time
+import random
+import asyncio
+from telegram import Bot
 
-PHOTO = '/home/serezha/images' # путь к папке с фото. На Windows может принимать иной вид, можно посмотреть в проводнике
-TEXT = '/home/serezha/texts' # путь к папке с текстом. /
-TOKEN = '6141726757:AAE_JhmM_GDkdhRa_jGjdsT3NQeoYdeEmro' # токен бота. Смотрим в @BotFather
+PHOTO = '/path'
+TEXT = '/path'
+TOKEN = 'xxxxxxxxxxxx-xxxxxxxxxx-999'
 
-bot = Bot(token=TOKEN) # инициализация объекта
+bot = Bot(token=TOKEN)
 
-photo = os.listdir(PHOTO) # список элементов папки с фото
-text = os.listdir(TEXT) #
+photo = os.listdir(PHOTO)
+text = os.listdir(TEXT)
  
-used_photo = [] # использованные фото
-used_text = [] # использованные текста
+used_photo = []
+used_text = []
 
-# def random_files(directory): # поиск пути случайного файла из выбранной директории (функция общего вида)
-#    files = os.listdir(directory) # создание списка из файлов директории
-#    random_file = random.choice(files) # выбор случайного файла
-#    file_path = os.path.join(directory, random_file) # путь файла в директории
-#    return file_path # функция возвращает искомый путь 
+# def random_files(directory):
+#    files = os.listdir(directory)
+#    random_file = random.choice(files)
+#    file_path = os.path.join(directory, random_file)
+#    return file_path 
 
-def unused(file_list, used_list): # функция, возвращающая список элементов, которые еще не были использованы в текущем цикле
-    unused_list = list(set(file_list) - set(used_list)) # разница списка всех элементов и списка использованных 
-    return unused_list # возвращаем список доступных для использования элементов
+def unused(file_list, used_list): 
+    unused_list = list(set(file_list) - set(used_list)) 
+    return unused_list 
 
-def random_photo_path(photo, used_photo): # функция случайного выбора фотографии
-    if len(used_photo) == len(photo): # если все элементы были использованы - завершается цикл и список недоступных файлов обнуляется
-        used_photo.clear() # обнуление (очищение)
-    unused_photo = unused(photo, used_photo) # сохраняем все доступные файлы
-    random_photo = random.choice(unused_photo) # выбираем случайный файл из доступных
-    used_photo.append(random_photo) # добавляем файл в использованные
-    photo_path = os.path.join(PHOTO, random_photo) # сохраняем путь выбранного файла
-    return photo_path # возвращаем путь
+def random_photo_path(photo, used_photo): 
+    if len(used_photo) == len(photo):
+        used_photo.clear() 
+    unused_photo = unused(photo, used_photo)
+    random_photo = random.choice(unused_photo) 
+    used_photo.append(random_photo) 
+    photo_path = os.path.join(PHOTO, random_photo)
+    return photo_path
 
-# функция полностью повторяет верхнуюю, но предназначена для работы с текстом, а не фото
+def random_text_path(text, used_text): 
+    if len(used_text) == len(text): 
+        used_text.clear() 
+    unused_text = unused(text, used_text) 
+    random_text = random.choice(unused_text) 
+    used_text.append(random_text) 
+    text_path = os.path.join(TEXT, random_text) 
+    return text_path 
 
-def random_text_path(text, used_text): #
-    if len(used_text) == len(text): #
-        used_text.clear() #
-    unused_text = unused(text, used_text) #
-    random_text = random.choice(unused_text) #
-    used_text.append(random_text) #
-    text_path = os.path.join(TEXT, random_text) #
-    return text_path #
+async def send(text, photo):
+    photo_path_file = random_photo_path(photo, used_photo) 
+    text_path_file = random_text_path(text, used_text)
+    with open(photo_path_file, 'rb') as photo_file, open(text_path_file, 'r') as text_file:
+        await bot.send_photo(chat_id='@testbottelegramapi', photo=photo_file, caption=text_file.read())
 
-async def send(text, photo): # функция выводa
-    photo_path_file = random_photo_path(photo, used_photo) # сохраняем пути случайных файлов (текста/фото) // random_files(PHOTO) 
-    text_path_file = random_text_path(text, used_text) # // random_files(TEXT)
-    with open(photo_path_file, 'rb') as photo_file, open(text_path_file, 'r') as text_file: # открытие файлов
-        await bot.send_photo(chat_id='@testbottelegramapi', photo=photo_file, caption=text_file.read()) # вывод файлов в одном потоке
-
-async def auto(): # бесконечный запуск
-    while True: # бесконечный цикл, будет работать всегда для вывода и установки задержки вывода
-        await send(text, photo) # вызов функции вывода == вывод
-        await asyncio.sleep(5) # интервал между выводом. Указывается в секундах. Например, для 3-х дней это значение будет равно 259200
-
-# постоянная структура с элементами асинхронного программирования
+async def auto(): 
+    while True: 
+        await send(text, photo)
+        await asyncio.sleep(5) # задержка вывода в секундах
 
 async def main():
     await auto()
